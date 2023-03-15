43e98c9fd9fa89d75ec2f87369f01b96f800d3be
ViktoriaSadivska <vika.sadivska@gmail.com>
2023-03-15 13:25:01 +0200

    added pluggin to embed instragram posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 const {DateTime} = require('luxon');
+const embedEverything = require("eleventy-plugin-embed-everything");
 
 module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
+    eleventyConfig.addPlugin(embedEverything);
 
     eleventyConfig.addFilter("postDate", (dateObj)=>{
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
