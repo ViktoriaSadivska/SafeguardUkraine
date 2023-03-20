e74fd0d60ea7d883752e660f33c2e1898d39aeb9
ViktoriaSadivska <vika.sadivska@gmail.com>
2023-03-20 15:32:58 +0200

    changed instagram pluggin settings

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
 const {DateTime} = require('luxon');
-const embedEverything = require("eleventy-plugin-embed-everything");
+const embedInstagram = require("eleventy-plugin-embed-instagram");
 
 module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
-    eleventyConfig.addPlugin(embedEverything);
+    eleventyConfig.addPlugin(embedInstagram);
 
     eleventyConfig.addFilter("postDate", (dateObj)=>{
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
